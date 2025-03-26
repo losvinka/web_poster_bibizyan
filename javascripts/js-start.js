70f13e878978dc6f3d7f26cd1b06593b5c1c3842
losvinka <tomapak@icloud.com>
2025-03-26 21:58:26 +0300

    first_screen

--- javascripts/js-start.js
+++ javascripts/js-start.js
@@ -1,1 +1,28 @@
-console.log("привет");
+document.addEventListener("DOMContentLoaded", () => {
+  changePrava();
+});
+
+function changePrava() {
+  let prava = document.querySelector(".prava-2");
+  let cnt = 1;
+
+  let urls = [
+    "images/batco.svg",
+    "images/bratok.svg",
+    "images/chichi.svg",
+    "images/cupan.svg",
+    "images/lianova.svg",
+    "images/polot.svg",
+    "images/bananov.svg",
+  ];
+
+  prava.addEventListener("click", () => {
+    prava.src = urls[cnt];
+    cnt++;
+    console.log(cnt);
+
+    if (cnt == 7) {
+      cnt = 0;
+    }
+  });
+}
