e5ac07ce0e617349fd2c240ec4e2e7a38d81d070
losvinka <tomapak@icloud.com>
2025-03-29 00:34:57 +0300

    poltoraecrana

--- javascripts/js-start.js
+++ javascripts/js-start.js
@@ -1,3 +1,4 @@
+// document.getElementById("changePrava()","changePrava()","changePrava()" ).style.cursor = "pointer";
 document.addEventListener("DOMContentLoaded", () => {
   changePrava();
 });
@@ -26,4 +27,75 @@ function changePrava() {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  changewordsbibi();
+});
+
+function changewordsbibi() {
+  let redbibizyan = document.querySelector(".redbibizyan");
+  let cnt = 1;
+
+  let urls = ["images/redbibi2.svg", "images/group1.svg"];
+
+  redbibizyan.addEventListener("click", () => {
+    redbibizyan.src = urls[cnt];
+    cnt++;
+    console.log(cnt);
+
+    if (cnt == 2) {
+      cnt = 0;
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  changewordsbibi2();
+});
+
+function changewordsbibi2() {
+  let bluebibizyan = document.querySelector(".bluebibizyan");
+  let cnt = 1;
+
+  let urls = ["images/group4.svg", "images/group2.svg"];
+
+  bluebibizyan.addEventListener("click", () => {
+    bluebibizyan.src = urls[cnt];
+    cnt++;
+    console.log(cnt);
+
+    if (cnt == 2) {
+      cnt = 0;
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  changeGlasses();
+});
+
+function changeGlasses() {
+  let prava = document.querySelector(".glasses");
+  let cnt = 1;
+
+  let urls = [
+    "images/glasses1.svg",
+    "images/glasses2.svg",
+    "images/glasses3.svg",
+    "images/glasses4.svg",
+    "images/glasses5.svg",
+    "images/glasses6.svg",
+  ];
+
+  prava.addEventListener("click", () => {
+    prava.src = urls[cnt];
+    cnt++;
+    console.log(cnt);
+
+    if (cnt == 6) {
+      cnt = 0;
+    }
+  });
+}
+
 // добавить курсор
